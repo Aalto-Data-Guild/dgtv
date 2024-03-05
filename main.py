3fbf27d4de0ecd178ca613164798ca84599309cc
Ilia Zalesskii <ilia.zalesskii@protonmail.com>
2024-03-05 19:08:54 +0200

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Page Title", layout="wide")
+
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
+st.title("DGTV")
+
+st.write("Some text")
