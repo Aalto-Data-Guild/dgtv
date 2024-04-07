9fe92c2376cfafef09a1febc3af4d19be1c1c915
lmalv <lassi.malvikko@aalto.fi>
2024-04-07 13:40:32 +0300

    added weather forecast plot

--- weather.py
+++ weather.py
@@ -7,8 +7,10 @@ from retry_requests import retry
 def widget():
     st.markdown("## Weather")
     weather_data = get_weather_data()
-    for data in weather_data:
-        st.markdown(data)
+
+    st.markdown("### Forecast")
+    forecast_chart_data = weather_data.rename(columns = {'date':'Time', 'temperature':'Temperature', 'precipitation':'Precipitation'})
+    st.line_chart(data = forecast_chart_data, x = 'Time', y = ['Temperature', 'Precipitation'], color = ['#63b8ff', '#ff4500'])
 
 def get_weather_data():
     cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
@@ -19,7 +21,7 @@ def get_weather_data():
     params = {
         "latitude": 60.184,
         "longitude": 24.8279,
-        "current": ["temperature_2m", "weather_code"],
+        "hourly": ["temperature_2m", "precipitation", "weather_code"],
         "timezone": "auto",
         "forecast_days": 1
     }
@@ -27,8 +29,22 @@ def get_weather_data():
 
     response = responses[0]
 
-    current = response.Current()
-    current_temperature_2m = current.Variables(0).Value()
-    current_weather_code = current.Variables(1).Value()
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+    hourly_weather_code = hourly.Variables(2).ValuesAsNumpy()
 
-    return [current_temperature_2m, current_weather_code]
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+
+    hourly_data["temperature"] = hourly_temperature_2m
+    hourly_data["precipitation"] = hourly_precipitation
+    hourly_data["weather_code"] = hourly_weather_code
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+
+    return hourly_dataframe
