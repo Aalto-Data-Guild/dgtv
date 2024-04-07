e617c2ae41bedff58606e4b1c9efbc6904b948bf
Anton Shumilin <70016123+Pelmenner@users.noreply.github.com>
2024-04-07 13:06:19 +0300

    Add restaurants widget (#1)
    * Add simple table with kanttiinit.fi menus
    
    * Customize menu list appearance
    
    * Handle closed restaurants
    
    * Update menu styles
    
    * Add A-Bloc
    
    * Remove menu title

--- restaurants.py
+++ restaurants.py
@@ -1,6 +1,48 @@
+import dataclasses
+import datetime
+from typing import Dict, List, Optional
+from jinja2 import Template
+
+import requests
 import streamlit as st
+
+KANTTIINIT_URL_MASK = "https://kitchen.kanttiinit.fi/menus?lang=en&restaurants={}&days={}"
+RESTAURANT_IDS = [2, 3, 52]
+RESTAURANT_NAMES = {2: "CS", 3: "Täffä", 52: "A-Bloc"}
+
+
+@dataclasses.dataclass
+class RestaurantResponse:
+    @dataclasses.dataclass
+    class MenuItem:
+        title: str
+        properties: Dict[int, str]
+
+    id: int
+    name: str
+    dates: Dict[str, Optional[List[MenuItem]]]
+
+
+def get_restaurants(date: datetime.date) -> List[RestaurantResponse]:
+    date_str = date.strftime("%Y-%m-%d")
+    restaurant_ids_str = ','.join(str(rid) for rid in RESTAURANT_IDS)
+
+    response = requests.get(KANTTIINIT_URL_MASK.format(restaurant_ids_str, date_str))
+    return [RestaurantResponse(int(id), RESTAURANT_NAMES.get(int(id), "Place"), restaurant_json) for (id, restaurant_json) in
+            response.json().items()]
 
 
 def widget():
-    st.text('restaurants')
+    today_date = datetime.datetime.today().date()
+    today_str = today_date.strftime("%Y-%m-%d")
+    with open("restaurants.html", "r") as f:
+        template = Template(f.read())
 
+    for restaurant in get_restaurants(today_date):
+        st.markdown(f"#### :green[{restaurant.name}]")
+        items = restaurant.dates.get(today_str)
+        if not items:
+            st.markdown("No menu available :cry:")
+            continue
+        menu = template.render(menu=items)
+        st.markdown(menu, unsafe_allow_html=True)
